e9048586988813673205669fc8915a59c9a13785
dianatoti <dianatoti10@gmail.com>
2026-01-15 11:25:47 +0100

    feat: organizzazione progetto, creato componenti (wip)

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+export default function Header() {
+    return (
+        <header>
+          <div>
+            <img src="/dc-logo.png" alt="Logo" />
+          </div>
+          <div>
+            <nav>
+          <ul>
+            <li><a href="">item</a></li>
+            <li><a href="">item</a></li>
+            <li><a href="">item</a></li>
+            <li><a href="">item</a></li>
+            <li><a href="">item</a></li>
+            <li><a href="">item</a></li>
+            <li><a href="">item</a></li>
+            <li><a href="">item</a></li>
+            <li><a href="">item</a></li>
+            <li><a href="">item</a></li>
+
+          </ul>
+            </nav>
+          </div>
+        </header>
+    )
+}
