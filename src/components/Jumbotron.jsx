0623425b1d39e640a0ee4b1f88e01136f7d3ad06
dianatoti <dianatoti10@gmail.com>
2026-02-09 17:56:46 +0100

    feat: creato componenti jumbotron e hero, modificato main e parte main di css

---
+++ src/components/Jumbotron.jsx
@@ -0,0 +1,12 @@
+export default function Jumbotron() {
+    return(
+        <section className="jumbotron">
+            <img src="/img/jumbotron.jpg" alt="DC Banner" />
+            <div className="container">
+                <button className="series-btn">
+                    CURRENT SERIES
+                </button>
+            </div>
+        </section>
+    )
+}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,3 @@
 export default function Main() {
-    return <main className="bg-main"> 
-        <h2> -- Content goes here -- </h2>
-    </main>;
+    
 }
