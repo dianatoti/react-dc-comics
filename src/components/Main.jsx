db1c24acc1060120521e3b2d482b1c2a7ea075b0
dianatoti <dianatoti10@gmail.com>
2026-02-09 23:38:49 +0100

    feat: aggiunto file js e strutturato main con griglia bootstrap

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,34 @@
+import comics from "./js/comics"
+
 export default function Main() {
-    
+    const comicsList = comics.map((curComic) => (
+        <div className="col-6 col-md-4 col-lg-2" key={curComic.id}>
+            <div className="card-img">
+                <img src={curComic.thumb} alt={curComic.id} />
+            </div>
+            <p className="card-title">
+                {curComic.series}
+            </p>
+        </div>
+    ));
+
+    return (
+        <main className="bg-dark py-5">
+            <div className="container">
+                <div className="row g-4">
+                    {comicsList}
+                </div>
+
+                <div className="text-center mt-4">
+                    <button className="btn btn-primary px-5">
+                        LOAD MORE
+                    </button>
+                </div>
+            </div>
+        </main>
+    );
 }
+
+
+
+
